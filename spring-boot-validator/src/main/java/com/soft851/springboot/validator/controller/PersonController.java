3d22fc74cce4f4caa8a9bdea0cd8e4319a7b1b4f
dyf <510040515@qq.com>
2020-04-30 22:56:01 +0800

    spring-boot-validator第二版

--- spring-boot-validator/src/main/java/com/soft851/springboot/validator/controller/PersonController.java
+++ spring-boot-validator/src/main/java/com/soft851/springboot/validator/controller/PersonController.java
@@ -1,11 +1,13 @@
 package com.soft851.springboot.validator.controller;
 
+import com.soft851.springboot.validator.annotation.ControllerWebLog;
 import com.soft851.springboot.validator.entity.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
 
@@ -16,8 +18,15 @@ import javax.validation.constraints.Size;
 public class PersonController {
 
     @PostMapping("/person")
-    public ResponseEntity<Person> savePerson(@RequestBody @Valid Person person){
-        log.info("入参对象："+person);
+    @ControllerWebLog(name = "getHello",isSaved = true)
+    public ResponseEntity<Person> savePerson(@RequestBody @Valid Person person, HttpServletRequest request){
+        log.info("控制层获得参数："+person);
+        log.info("控制层获得参数："+request);
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return ResponseEntity.ok().body(person);
     }
 
